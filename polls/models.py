c35261629c66fd8e9c35975045931d5de867e52a
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-08-25 18:42:08 -0700

    Type: Views Updated with more views.

--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,11 @@
-import datetime
-
-from django.db import models
-from django.utils import timezone
-
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
+import datetime
+from django.utils import timezone
 # Create your models here.
 @python_2_unicode_compatible
 class Question(models.Model):
